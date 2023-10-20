45118c5fa2d6bdafb1d9b67b13c2b886d86205ce
Tegardf <zoel.tegar234@gmail.com>
2023-10-20 22:37:47 +0700

    update: content responsive fiks
    -logic for login or not
    -using bootstrap for content responsive
    -add link for news

--- script/homepage.js
+++ script/homepage.js
@@ -1,49 +1,49 @@
-function handleGetFormData() {
-    let valueInput = {};
+const URL_API_PROFIL = 'https://6530a73e6c756603295ee17b.mockapi.io/profil'
+const winWidth = window.innerWidth;
+let brClassSel = '';
 
-    valueInput.name = document.getElementById('name').value; 
-    valueInput.email = document.getElementById('email').value;
-    valueInput.city = document.getElementById('city').value;  
-    valueInput.zipCode = document.getElementById('zip-code').value; 
-    valueInput.status = document.getElementById('status').checked; 
-
-    return valueInput;
+if (winWidth<=992) {
+    brClassSel = 'mobile-'
+} else {
+    brClassSel = 'desktop-'
 }
 
-function isNumber(data) {
-    return !isNaN(data);
+async function getDataFrom(localData) {
+    const response = await fetch(URL_API_PROFIL)
+    const userData = await response.json()
+
+    newData = userData.filter((data)=>data.email==localData)[0]
+    document.querySelector('#nav-profil-img').src = newData.avatar
+    document.querySelector('#profil-img').src = newData.avatar
+    document.querySelector('#profil-name').innerHTML = newData.name
 }
 
-function checkboxIsChecked(){
-    let cek = document.getElementById('status').checked;
-    if(cek) return true;
-    else false;
-}
-function validateFormData(data){
-    if(data !== null && isNumber(data.zipCode) && checkboxIsChecked()) return true;
-    else false;
-}
-
-document.getElementById('submit-form').addEventListener("click", submit);
-
-function submit(event){
-    event.preventDefault();
-    let container=document.getElementById('warning');
-    if(validateFormData(handleGetFormData())) container.replaceChildren();
-    else container.innerHTML = '<p>Periksa form anda sekali lagi</p>';
-}
-
-function togglePopup() {
-    let popup = document.getElementById("popup-container");
-    popup.style.display = (popup.style.display === "block") ? "none" : "block";
-}
-
-document.addEventListener("click", function(event) {
-    let popup = document.getElementById("popup-container");
-    let button = document.querySelector(".float-button");
-
-    if (event.target !== popup && event.target !== button && !popup.contains(event.target)) {
-        popup.style.display = "none";
+function checkStateLogin() {
+    const data = localStorage.getItem('email');
+    if (winWidth<=992) {
+        if (data) {
+            getDataFrom(localData=data)
+            document.querySelector('#'+brClassSel+'profilOn').className = "nav-item "
+            document.querySelector('#'+brClassSel+'profilOff').className = "nav-item d-none"
+        } else {
+            document.querySelector('#'+brClassSel+'profilOn').className = "nav-item d-none"
+            document.querySelector('#'+brClassSel+'profilOff').className = "nav-item"
+        }   
+    } else {
+        if (data) {
+            getDataFrom(localData=data)
+            document.querySelector('#'+brClassSel+'profilOn').className = "nav-item ps-3 pe-5 "
+            document.querySelector('#'+brClassSel+'profilOff').className = "nav-item ps-3 pe-3 d-flex flex-row align-items-center gap-2 d-none"
+        } else {
+            document.querySelector('#'+brClassSel+'profilOn').className = "nav-item ps-3 pe-5 d-none"
+            document.querySelector('#'+brClassSel+'profilOff').className = "nav-item ps-3 pe-3 d-flex flex-row align-items-center gap-2"
+        }    
     }
-});
+}
+checkStateLogin()
+
+document.querySelector('#'+brClassSel+'btn-logout').addEventListener("click",()=>{
+    localStorage.removeItem("email");
+    window.location.href = 'index.html'
+})
 
