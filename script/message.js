43144ea5c0a91d42d93ef965aebd747c525ea7a1
Tegardf <zoel.tegar234@gmail.com>
2023-10-20 16:23:17 +0700

    update: sync fitur pesan
    -fetch 2 data (admin and konselor) from mockAPI
    -connect fitur message list with body of message
    -content and fitur message page done

--- script/message.js
+++ script/message.js
@@ -1,7 +1,18 @@
 const URL_API_PROFIL = 'https://6530a73e6c756603295ee17b.mockapi.io/profil'
-let newData;
+let newDataKonselor;
 const winWidth = window.innerWidth;
 let brClassSel = '';
+let role;
+let dataS = [];
+let contentPesanKonselor = [
+    {masuk:'Halo apa Kabar', keluar:''},
+    {masuk:'Bagaimana Keadanmu?', keluar:''},
+]
+
+let contentPesanAdmin = [
+    {masuk:'Halo apa Kabar', keluar:''},
+    {masuk:'Perkenalkan Saya Robert, Admin dari website yang akan memandu kamu', keluar:''},
+]
 
 if (winWidth<=992) {
     brClassSel = 'mobile-'
@@ -13,18 +24,18 @@ async function getDataFrom(localData) {
     const response = await fetch(URL_API_PROFIL)
     const userData = await response.json()
     
-    newData = userData.filter((data)=>data.email==localData)[0]
-    document.querySelector('#nav-profil-img').src = newData.avatar
-    document.querySelector('#profil-img').src = newData.avatar
-    document.querySelector('#desktop-profil-sdbar').src = newData.avatar
-    document.querySelector('#profil-name').innerHTML = newData.name
+    newDataKonselor = userData.filter((data)=>data.email==localData)[0]
+    document.querySelector('#nav-profil-img').src = newDataKonselor.avatar
+    document.querySelector('#profil-img').src = newDataKonselor.avatar
+    document.querySelector('#desktop-profil-sdbar').src = newDataKonselor.avatar
+    document.querySelector('#profil-name').innerHTML = newDataKonselor.name
     // console.log(document.querySelector('#desktop-profil-sdbar'))
 }
 function addNewMessageList(data) {
     let element = ''
     if (winWidth<=992) {
-        data.forEach((dataPengirim) => {
-            element = element+`<button class="btn rounded-start-5 p-2 shadow" style="--bs-btn-border-width:0;" data-bs-toggle="offcanvas" data-bs-target="#db-message" aria-controls="db-message" aria-label="Toggle docs navigation" >
+        data.forEach((dataPengirim,index) => {
+            element = element+`<button class="btn rounded-start-5 p-2 shadow" style="--bs-btn-border-width:0;" data-bs-toggle="offcanvas" data-bs-target="#db-message" aria-controls="db-message" aria-label="Toggle docs navigation"  id="mobile-pesanKe-${index+1}" onclick="pesanClicked(event)" >
                                     <div class="d-flex flex-row align-items-center gap-4 p-2" >
                                         <img src="${dataPengirim.avatar}" class="rounded-circle" style="width: 60px; height: 60px;" alt="">
                                         <div class="d-flex flex-column align-items-start gap-1">
@@ -36,8 +47,8 @@ function addNewMessageList(data) {
             
         });    
     }else{
-        data.forEach((dataPengirim) => {
-            element = element+`<button class="btn rounded-start-5 p-2 shadow" style="--bs-btn-border-width:0;" data-bs-toggle="" data-bs-target="" aria-controls="" aria-label="" >
+        data.forEach((dataPengirim,index) => {
+            element = element+`<button class="btn rounded-start-5 p-2 shadow" style="--bs-btn-border-width:0;" data-bs-toggle="" data-bs-target="" aria-controls="" aria-label=""  id="desktop-pesanKe-${index+1}" onclick="pesanClicked(event)">
                                     <div class="d-flex flex-row align-items-center gap-4 p-2" >
                                         <img src="${dataPengirim.avatar}" class="rounded-circle" style="width: 60px; height: 60px;" alt="">
                                         <div class="d-flex flex-column align-items-start gap-1">
@@ -58,16 +69,34 @@ async function getDataKonselor() {
     const response = await fetch(URL_API_PROFIL+'/3')
     const konselorData = await response.json()
 
-    let dataS = []
+    
     let dataList = {
         nama:konselorData.name,
         avatar:konselorData.avatar,
-        isi:"Halo apa Kabar "
+        isi:konselorData.role
     }
     dataS.push(dataList)
     // dataS.push(dataList)
 
     addNewMessageList(dataS)
+    // changeHeaderPesan(konselorData.avatar,konselorData.name,konselorData.role)
+    // console.log(userData)
+}
+async function getDataAdmin() {
+    const response = await fetch(URL_API_PROFIL+'/1')
+    const adminData = await response.json()
+
+    
+    let dataList = {
+        nama:adminData.name,
+        avatar:adminData.avatar,
+        isi:adminData.role
+    }
+    dataS.push(dataList)
+    // dataS.push(dataList)
+
+    addNewMessageList(dataS)
+    // changeHeaderPesan(konselorData.avatar,konselorData.name,konselorData.role)
     // console.log(userData)
 }
 
@@ -79,9 +108,86 @@ function checkStateLogin() {
         window.location.href = 'index.html';
     }
 }
+function stringPesanMasuk(pesan=''){
+    if (pesan=='') {
+        return ''
+    }
+    return (`<div class="d-flex justify-content-start ps-4" id="pesan-masuk">
+                <div class="rounded-4 ps-2 pe-2" style="max-width: 80%; background-color: var(--color11);">
+                    <p class="m-2">${pesan}</p>
+                </div>
+            </div>
+            `)
+}
+
+function stringPesanKeluar(pesan=''){
+    if (pesan=='') {
+        return ''
+    }
+    return (`<div class="d-flex justify-content-end pe-4" id="pesan-keluar">
+                <div class="rounded-4 ps-2 pe-2" style="max-width: 80%; background-color: var(--color12);">
+                    <p class="m-2">${pesan}</p>
+                </div>
+            </div>
+            `)
+}
+function changeHeaderPesan(url,nama,role) {
+    document.querySelector('#'+brClassSel+'header-img').src = url
+    document.querySelector('#'+brClassSel+'header-nama').innerHTML = nama
+    document.querySelector('#'+brClassSel+'header-role').innerHTML = role
+}
+async function renderPesan() {
+    let list = document.querySelector('#'+brClassSel+'message-body')
+    list.innerHTML = ''
+    let dataNow = list.innerHTML
+    if (role =='admin') {
+        contentPesanAdmin.forEach((pesan)=>{
+            dataNow = dataNow + stringPesanMasuk(pesan.masuk) +stringPesanKeluar(pesan.keluar)
+        })
+    } else if(role =='konselor') {
+        contentPesanKonselor.forEach((pesan)=>{
+            dataNow = dataNow + stringPesanMasuk(pesan.masuk) +stringPesanKeluar(pesan.keluar)
+        })
+    }
+    
+    list.innerHTML = dataNow   
+}
+function pesanClicked(e){
+    const idTarget = e.currentTarget.id
+    // const idNum = idTarget[idTarget.length-1]
+    let url,nama;
+    url = document.querySelector('#'+idTarget+' img').src
+    nama = document.querySelector('#'+idTarget+' h5').textContent
+    role = document.querySelector('#'+idTarget+' p').textContent
+    // console.log()
+    changeHeaderPesan(url,nama,role)
+    if (winWidth>992) {
+        document.querySelector('#content-message').className = 'col-6 col-xl-7 ps-0 d-flex flex-column'
+    }
+
+    renderPesan()
+
+}
 
 checkStateLogin()
 getDataKonselor()
+getDataAdmin()
+
+document.querySelector('#desktop-close-message').addEventListener("click",()=>{
+    if (winWidth>992) {
+        document.querySelector('#content-message').className = 'col-6 col-xl-7 ps-0 d-flex flex-column d-none'
+    }
+})
+document.querySelector('#'+brClassSel+'sendBtn').addEventListener("click",()=>{
+    let inputMessage =  document.querySelector('#'+brClassSel+'message-input #input-text')
+    if (role == 'admin' ) {
+        contentPesanAdmin.push({masuk:'',keluar:inputMessage.value})
+    } else if(role =='konselor') {
+        contentPesanKonselor.push({masuk:'',keluar:inputMessage.value})
+    }
+    renderPesan()
+    inputMessage.value = ''
+})
 
 document.querySelector('#'+brClassSel+'btn-logout').addEventListener("click",()=>{
     localStorage.removeItem("email");
